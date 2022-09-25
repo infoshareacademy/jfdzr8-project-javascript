499890c3f5e6d36a9e12f3cecffc82df969f137a
Ewelina <ebialoszycka@gmail.com>
2022-09-25 16:37:41 +0200

    1-2-3 challenge
    Co-authored-by: Aleksander Kulej <aleksanderkulej@users.noreply.github.com>
    Co-authored-by: Jakub Bedynek <JackRaven611@users.noreply.github.com>

---
+++ 03-dom/team2/index.js
@@ -0,0 +1,18 @@
+//ex. 1
+
+const header = document.getElementById("header");
+
+console.log(header);
+
+//ex. 2
+
+header.classList.add("header");
+
+
+//ex. 3
+
+header.innerText = "Jestem nagłówkiem"
+
+
+//ex. 4
+
